9917901fb21a0ee2efacabd82e5a94e301c8c0fc
NotRowan33 <rowanwolfe33@gmail.com>
2025-01-23 10:54:46 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-document.getElementById('alertButton').addEventListener('click', function() {
-    alert('Button clicked!');
-  });  
+const track = document.getElementById("image-track");
+
+window.onmousedown = e => {
+    track.dataset.mouseDownAt = e.clientX;
+
+
+window.onmousemove = e => {
+    const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX,
+        maxDelta = window.innerWidth / 2;
+
+    const percentage = (mouseDelta / maxDelta) * 100;
+}
