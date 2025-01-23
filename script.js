8979936a98eedb970d7254ab93c4578715b16285
NotRowan33 <rowanwolfe33@gmail.com>
2025-01-23 11:00:24 -0500

    Update script.js

--- script.js
+++ script.js
@@ -5,8 +5,12 @@ window.onmousedown = e => {
 
 
 window.onmousemove = e => {
+    if(track.dataset.mouseDownAt === "0") return;
+    
     const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX,
         maxDelta = window.innerWidth / 2;
 
     const percentage = (mouseDelta / maxDelta) * 100;
+
+    track.style.transform = `translate(${percentage}%, -50%)`;
 }
